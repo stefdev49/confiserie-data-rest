3ae266269e84f84bf8c87868d2375310c72eb9c3
Stéphane Voltz <stef.dev@free.fr>
2016-06-28 06:14:47 +0200

    convertion liste d'ids en liste de magasins dans la recherche textuelle

--- src/main/java/fr/stef/jug/repositories/MagasinRepositoryImpl.java
+++ src/main/java/fr/stef/jug/repositories/MagasinRepositoryImpl.java
@@ -1,44 +1,49 @@
 package fr.stef.jug.repositories;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceProperty;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 
 import fr.stef.jug.domain.Magasin;
 
 /**
- * Classe d'implémentation des comportement spécifiques définis dans MagasinRepositoryCustom.
- * Pour être utilisé comme implémentation de l'extension XxxxRepositoryCustom, la classe <b>doit s'appeler XxxxRepositoryImpl</b>.
- * Custom -> Impl
+ * Classe d'implémentation des comportement spécifiques définis dans
+ * MagasinRepositoryCustom. Pour être utilisé comme implémentation de
+ * l'extension XxxxRepositoryCustom, la classe <b>doit s'appeler
+ * XxxxRepositoryImpl</b>. Custom -> Impl
+ * 
  * @author stef
  *
  */
 public class MagasinRepositoryImpl implements MagasinRepositoryCustom {
-  @PersistenceContext(type = javax.persistence.PersistenceContextType.EXTENDED, properties = @PersistenceProperty(name = "org.hibernate.flushMode", value = "COMMIT"))
-  public EntityManager entityManager;
-  
   /**
    * Logger de la classe d'implémentation
    */
-  Logger LOGGER=LoggerFactory.getLogger(MagasinRepositoryImpl.class);
-  
+  private static final Logger LOGGER = LoggerFactory.getLogger(MagasinRepositoryImpl.class);
+
+  @Autowired
+  MagasinRepository magasins;
+
   @Override
   public List<Magasin> findByTexte(@Param("texte") String texte) {
     LOGGER.trace("Recherche par texte avec '{}'", texte);
-    
-    List<Magasin> result=new ArrayList<>();
-    Magasin un=new Magasin();
-    un.setNom("mock");
-    un.setDescription("magasin mock");
-    result.add(un);
-    return result;
+
+    /*
+     * on sélectionn les ids sur la base du texte donné, acutellement on simule
+     * ça avec une liste en dur en attendant que la recherche soit créée
+     */
+    List<Long> ids = Arrays.asList(1L, 2L);
+    LOGGER.trace("Les ids correspondants sont {}", ids);
+
+    /*
+     * on peut renvoyer maitnenant les magasins correspondants aux ids
+     * sélectionnés
+     */
+    return magasins.findByIdIn(ids);
   }
 
 }
